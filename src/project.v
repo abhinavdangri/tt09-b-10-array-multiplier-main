72b26ab808f597000ab6bc210c2f4e8f54cf5416
abhinavdangri <44752159+abhinavdangri@users.noreply.github.com>
2024-11-08 13:34:32 -0500

    s

--- src/project.v
+++ src/project.v
@@ -1,8 +1,11 @@
-
+/*
+ * Copyright (c) 2024 Hanyuan (Bob) Huang
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 `default_nettype none
 
-module tt_um_b_10_array_multiplier(
+module tt_um_b_8_array_multiplier(
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
