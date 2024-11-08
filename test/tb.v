14bd5076e831bb7e57c0937e175519b027fa02ca
abhinavdangri <44752159+abhinavdangri@users.noreply.github.com>
2024-11-08 13:45:39 -0500

    Try

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_b_8_array_multiplier user_project (
+  tt_um_b_10_array_multiplier user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
